0adffe3466cfa137556e0e1ea6f9e26ffecd4202
Daniel Duran <danields0734@gmail.com>
2023-03-02 18:12:47 +0100

    TODO: Change text for images

---
+++ app/src/main/java/com/example/stario/Affirmation.kt
@@ -0,0 +1,5 @@
+package com.example.stario
+
+data class Affirmation(val stringResourceId: Int){
+
+}
--- app/src/main/java/com/example/stario/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/stario/adapter/ItemAdapter.kt
@@ -1,13 +1,31 @@
 package com.example.stario.adapter
 
 import android.content.Context
-import android.view.TextureView
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.stario.Affirmation
 import com.example.stario.R
 
-class ItemAdapter (private val context: Context) {
+class ItemAdapter (private val context: Context, private val dataset: List<Affirmation>)
+    : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextureView = view.findViewById(R.id.item_title)
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return  ItemViewHolder(adapterLayout)
+    }
+
+    override fun getItemCount() = dataset.size
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
     }
 }
